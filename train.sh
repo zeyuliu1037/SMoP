7e1ecf0bae236fc20082b049c9a972bc5cd89b2c
zeyuliu1037 <liuzeyu@usc.edu>
2024-10-10 14:57:03 -0700

    move get_huggingface_superglue.py back to data/superglue/

--- train.sh
+++ train.sh
@@ -1,2 +1,2 @@
-CUDA_VISIBLE_DEVICES=5
+CUDA_VISIBLE_DEVICES=0
 python scripts/train.py --lr 0.5  --batch_size 32  --epoch 50 --max_length 512  --model_name_or_path t5-base --tokenizer_name_or_path t5-base --warmup_ratio 0.06 --method prompt-routing --dataset_name rte_superglue --num_virtual_tokens 5 --num_virtual_tokens_full 20 --perturb_router True --topk 1
