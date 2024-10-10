6046846bee1a90522fd306dffa084aa8b354970b
Wen Ye <wye@dt-login04.delta.ncsa.illinois.edu>
2024-10-10 13:41:14 -0500

    restore dataset download .py

---
+++ get_huggingface_superglue.py
@@ -0,0 +1,22 @@
+import os
+import sys
+if "SMoP" not in os.getcwd():
+    os.chdir("SMoP")
+from datasets import load_dataset, list_datasets
+import pickle
+
+# CoLA SST MRPC STS QQP MNLI QNLI RTE
+
+dataset_names = ['boolq', 'cb', 'copa', 'multirc', 'rte', 'wic']
+
+for dataset in dataset_names:
+    os.makedirs(f"data/superglue/{dataset}", exist_ok=True)
+    data = load_dataset('super_glue', dataset, trust_remote_code=True)
+    for key in data.keys():
+        path = f"data/superglue/{dataset}/{key}.pkl"
+        # d = load_dataset('glue', dataset)[key]
+        # for x in d:
+        #     print(x)
+        pickle.dump(data[key], open(path, 'wb'))
+
+
