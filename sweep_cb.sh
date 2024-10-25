e2d17c196aa7fba36494ea62f7b0c6ff6df46968
chenghan0516 <chenghan0516@gmail.com>
2024-10-25 01:14:28 -0700

    minor change to sweep_db.sh

--- sweep_cb.sh
+++ sweep_cb.sh
@@ -1,4 +1,5 @@
 source SMoPVenv/bin/activate
+CUDA_VISIBLE_DEVICES=0
 for lr in 0.3 0.1
 do
     python3 scripts/train.py --lr $lr  --batch_size 32  --epoch 10 --max_length 512  --model_name_or_path t5-base --tokenizer_name_or_path t5-base --warmup_ratio 0.06 --method prompt-routing --dataset_name cb --num_virtual_tokens 5 --num_virtual_tokens_full 20 --perturb_router False --topk 1
